49801800b245331695bb26debde104a2cd2adf74
Zoey Burke <zoeyelizabethburke@gmail.com>
2022-10-10 23:46:01 -0400

    go fmt

--- ch1/fetch.go
+++ ch1/fetch.go
@@ -2,15 +2,15 @@
 package main
 
 import (
-  "fmt"
-  "io/ioutil"
-  "net/http"
-  "os"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
 )
 
 func main() {
-  for _, url := range os.Args[1:] {
-    resp, err := http.Get(url)
+	for _, url := range os.Args[1:] {
+		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
 			os.Exit(1)
@@ -22,5 +22,5 @@ func main() {
 			os.Exit(1)
 		}
 		fmt.Printf("%s", b)
-  }
+	}
 }
