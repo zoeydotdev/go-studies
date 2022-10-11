559fc89ba3fa168f5d34ddba2bb5427f44f806f5
Zoey Burke <herself@zoey.cx>
2022-10-10 23:46:01 -0400

    go fmt

--- ch1/server1.go
+++ ch1/server1.go
@@ -2,17 +2,17 @@
 package main
 
 import (
-  "fmt"
-  "log"
-  "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func main() {
-  http.HandleFunc("/", handler) // each request calls handler
-  log.Fatal(http.ListenAndServe("localhost:80", nil))
+	http.HandleFunc("/", handler) // each request calls handler
+	log.Fatal(http.ListenAndServe("localhost:80", nil))
 }
 
 // handler echoes the Path component of the requested URL
 func handler(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "URL.Path = %q", r.URL.Path)
+	fmt.Fprintf(w, "URL.Path = %q", r.URL.Path)
 }
